e394eba42013e588868ed94a8a5ee884fd55889a
Ivet13 <jazzjackrabbit13@gmail.com>
2020-05-19 19:44:29 +0200

    Version mejorada actualizada muy buena

--- js/coche.js
+++ js/coche.js
@@ -1,25 +1,58 @@
 'use strict'
 
-/* creamos un objeto donde podemos meter info relacionada con el usuario y las marcas de coches que nos
-indicara haber tenido entrando datos en la ventanita de prompt. Ademas anadimos una funcion que nos muestra por pantalla
-una frase rellenada con las variables que nos haya pasado. */
-
-function coches () {
-  var historiaDeCochesDelUsuario = {
-    nombreUsuario: prompt('Tu nombre:'),
-    marca1: prompt('Introduce la primera marca de coche que ya has tenido:'),
-    marca2: prompt('Introduce la segunda marca de coche que ya has tenido:'),
-    marca3: prompt('Introduce la tercera marca de coche que ya has tenido:'),
-    mostrarDatos: function () {
-      var resultado = `Tu nombre es ${this.nombreUsuario} y ya has tenido coches de marcas: ${this.marca1}, ${this.marca2} y ${this.marca3}.` 
-      console.log(resultado)
-      document.write(resultado)
-    }
-  }
-
-  // aqui invocamos el metodo del objeto que hemos creado para que nos imprime la frase en la consola
-  historiaDeCochesDelUsuario.mostrarDatos()
+// para poder crear objetos de tipo Coche, nos creamos un constructor de este objeto
+function Coche (marca, ano, color) {
+  this.marca = marca,
+  this.ano = ano,
+  this.color = color
 }
 
-// esto sirve para lanzar la funccion al abrir la pagina
-coches()
+// tambien nos preparamos un array que acojera las instancias de nuestros coches
+var cochesDelUsuario = []
+
+// esta es la funcion principal
+function insertarCocheEnHistorial () {
+  // variable auxiliar para poder evaluar si continuar creando coches o si el usuario ha escrito "no" en el prompt o le ha dado al cancel
+  var para = false
+
+  // hasta que el usuario quiera crear coches
+  while (!para) {
+    var cocheNuevo = new Coche(
+      // directamente populamos la instancia nueva del objeto con valores que nos pasa el usuario mediante los prompts
+      prompt('Introduce la marca de coche que ya has tenido:'),
+      prompt('Cual fue el ano de su lanzamiento:'),
+      prompt('De que color era?')
+    )
+    console.log('creando coche')
+
+    // populamos el array con el coche que acabamos de crear
+    cochesDelUsuario.push(cocheNuevo)
+
+    console.log(cochesDelUsuario)
+
+    // despues de la creacion de una instancia del objeto vamos a preguntar si seguimos con otro coche o si paramos el programa
+    var quiereInsertarCocheNuevo = prompt('Quieres insertar un coche nuevo?')
+    if (quiereInsertarCocheNuevo === null || quiereInsertarCocheNuevo === 'no') {
+      para = true
+    } else {
+      para = false
+    }
+  }
+  // esta funcion sirve para anadir un nuevo elemento HTML, concretamente un parrafo y llenarlo con los valores de las propiedades de
+  // los objetos creados
+  crearNuevaCaja(cochesDelUsuario)
+  console.log('fin del codigo')
+}
+
+// esto sirve para arrancar la funcion principal
+insertarCocheEnHistorial()
+
+// aqui he empezado a probar como podriamos anadir datos de los objetos creados a parrafos nuevos en nuestra pagina index
+function crearNuevaCaja (coches) {
+  for (let i = 0; i < coches.length; i++) {
+    const cochesArticulo = document.getElementById('coches')
+    const nuevoParrafo = document.createElement('p')
+    cochesArticulo.appendChild(nuevoParrafo)
+    nuevoParrafo.innerHTML = `Marca: ${coches[i].marca} Ano: ${coches[i].ano} Color: ${coches[i].color}`
+  }
+}
